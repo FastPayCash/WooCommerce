d08a1d4225cbfdca2189b539afd4a92753b2d131
Mahfuzul Alam <mahfuzcse12@gmail.com>
2017-09-14 15:05:17 +0600

    Changes in google currency conversion API

--- index.php
+++ index.php
@@ -148,7 +148,7 @@ $this -> testValidationURL  = 'https://dev.fast-pay.cash/merchant/payment/valida
 $amount = $order -> order_total;
 $from = get_option('woocommerce_currency');
 $to = 'IQD';
-$url  = "https://www.google.com/finance/converter?a=$amount&from=$from&to=$to";
+$url  = "https://finance.google.com/finance/converter?a=$amount&from=$from&to=$to";
     $data = file_get_contents($url);
     preg_match("/<span class=bld>(.*)<\/span>/",$data, $converted);
     $converted = preg_replace("/[^0-9.]/", "", $converted[1]);
